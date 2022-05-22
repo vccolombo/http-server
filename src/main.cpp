1e153598857ea4f8211684e196f4495dd09d0ec0
Víctor Colombo <victorcora98@gmail.com>
2022-05-21 21:49:27 -0300

    Implement basic callback system to register routes
    Now the user will be able to register routes for the server
    
    For now, it's only possible to responde with strings. Later, reading
    from files and serving directories will be implemented

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,97 @@
 #include <iostream>
+#include <map>
 #include <memory>
+#include <utility>
 
 #include "server.hpp"
-// fn callback(HTTPParams& params) {}
-//
 
 namespace httpserver
 {
+
+enum class Method
+{
+    GET,
+    HEAD,
+    POST,
+    PUT,
+    DELETE,
+    INVALID,
+};
+
+struct HeaderParams
+{
+    Method method;
+    std::string path;
+};
+
+struct Request
+{
+    HeaderParams header;
+    std::string body;
+};
+
+class Response
+{
+   public:
+    using status_code_t = uint16_t;
+
+    Response() : status_code_(200) {}
+
+    void set_status(status_code_t status_code)
+    {
+        status_code_ = status_code;
+    }
+
+    void send(const std::string& data)
+    {
+        data_ = data;
+    }
+
+    void send_file(const std::string& path){
+        // TODO
+    };
+
+    [[nodiscard]] status_code_t status() const
+    {
+        return status_code_;
+    }
+
+    [[nodiscard]] std::string data() const
+    {
+        return data_;
+    }
+
+   private:
+    status_code_t status_code_;
+    std::string data_;
+};
+
+class Router
+{
+   public:
+    void register_get(
+        const std::string& path, const std::function<void(Request&, Response&)>& callback)
+    {
+        getters_[path] = callback;
+    }
+
+    std::function<void(Request&, Response&)> get(const std::string& path)
+    {
+        // I'm presuming this returns an empty function if it was not registered before. Need to
+        // test but it's still a bad idea, as it'll increase the size of the map. So:
+        // TODO: check if key exists
+        return getters_[path];
+    }
+
+   private:
+    std::map<std::string, std::function<void(Request&, Response&)>> getters_;
+};
+
 class HTTPServer final : public Application
 {
    public:
+    explicit HTTPServer(Router& router) : router_(router) {}
+
     bool on_data(uint8_t* data, std::size_t length) override
     {
         // https://stackoverflow.com/questions/4508911/convert-uint8-t-to-stdstring-in-c
@@ -19,10 +101,32 @@ class HTTPServer final : public Application
         auto header_params = parse_header(request_text);
         std::cout << header_params.path << "\n";
 
+        Request req = {.header = header_params, .body = ""};
+        Response res;
+
         if (header_params.method == Method::GET)
         {
-            char response[] = "Hello world";
-            writer_->write(reinterpret_cast<uint8_t*>(response), strlen(response));
+            auto callback = router_.get(header_params.path);
+            if (callback)
+            {
+                callback(req, res);
+
+                auto data = res.data();
+                auto data_len = data.length();
+
+                // TODO: create a class to wrap this buffer and allow any size
+                char buf[1024 + data_len];
+                snprintf(buf, sizeof(buf),
+                    "HTTP/1.1 %d OK\r\nContent-Length: %lu\r\nContent-Type: text/html\r\n\r\n%s",
+                    res.status(), data_len, data.c_str());
+                writer_->write(reinterpret_cast<uint8_t*>(buf), strlen(buf));
+            }
+            else
+            {
+                char buf[4096];
+                snprintf(buf, sizeof(buf), "HTTP/1.1 %d\r\nContent-Length: 0\r\n\r\n", 404);
+                writer_->write(reinterpret_cast<uint8_t*>(buf), strlen(buf));
+            }
         }
         else
         {
@@ -32,29 +136,7 @@ class HTTPServer final : public Application
         return false;
     }
 
-    //    // register get
-    //    void get(path, callback);
-    //
-    //    // register post
-    //    void post();
-
    private:
-    enum class Method
-    {
-        GET,
-        HEAD,
-        POST,
-        PUT,
-        DELETE,
-        INVALID,
-    };
-
-    struct HeaderParams
-    {
-        Method method;
-        std::string path;
-    };
-
     struct HeaderParams parse_header(const std::string& request_text)
     {
         return HeaderParams{
@@ -96,19 +178,30 @@ class HTTPServer final : public Application
         auto full_query = request_text.substr(start_pos, length);
         return full_query;
     }
+
+    Router& router_;
 };
 
 class HTTPServerFactory final : public ApplicationFactory
 {
    public:
+    explicit HTTPServerFactory(Router& router) : router_(router) {}
+
     Application_ptr create() override
     {
-        return std::make_unique<HTTPServer>();
+        return std::make_unique<HTTPServer>(router_);
     }
 
    private:
+    Router& router_;
 };
 }  // namespace httpserver
+
+void callback_index(httpserver::Request& req, httpserver::Response& res)
+{
+    res.set_status(200);
+    res.send("Hello World!!\r\nThis is a sample app for my http server\r\n;)");
+}
 
 int main(int argc, char* argv[])
 {
@@ -117,9 +210,9 @@ int main(int argc, char* argv[])
     httpserver::io_context io_context;
     httpserver::tcp::endpoint endpoint(httpserver::tcp::v4(), port);
 
-    //    Router router;
-    //    router.get("/", callback);
-    httpserver::HTTPServerFactory http_server_factory;
+    httpserver::Router router;
+    router.register_get("/", callback_index);
+    httpserver::HTTPServerFactory http_server_factory(router);
     httpserver::TCPServer server(io_context, endpoint, http_server_factory);
 
     io_context.run();
