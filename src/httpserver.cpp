bdd37c8c314b39d7d0625c4234134967403a743a
Víctor Colombo <victorcora98@gmail.com>
2022-05-21 22:32:56 -0300

    Return "not implemented 501" response to not implemented features

--- src/httpserver.cpp
+++ src/httpserver.cpp
@@ -22,19 +22,19 @@ bool HTTPServer::on_data(uint8_t* data, std::size_t length)
         {
             callback(req, res);
 
-            auto data = res.data();
-            auto data_len = data.length();
+            auto response_body = res.data();
+            auto response_body_len = response_body.length();
 
             // TODO: create a class to wrap this buffer and allow any size
-            char buf[1024 + data_len];
+            char buf[1024 + response_body_len];
             snprintf(buf, sizeof(buf),
                 "HTTP/1.1 %d OK\r\nContent-Length: %lu\r\nContent-Type: text/html\r\n\r\n%s",
-                res.status(), data_len, data.c_str());
+                res.status(), response_body_len, response_body.c_str());
             writer_->write(reinterpret_cast<uint8_t*>(buf), strlen(buf));
         }
         else
         {
-            char buf[4096];
+            char buf[64];
             snprintf(buf, sizeof(buf), "HTTP/1.1 %d\r\nContent-Length: 0\r\n\r\n", 404);
             writer_->write(reinterpret_cast<uint8_t*>(buf), strlen(buf));
         }
@@ -42,6 +42,9 @@ bool HTTPServer::on_data(uint8_t* data, std::size_t length)
     else
     {
         // TODO: not implemented
+        char buf[64];
+        snprintf(buf, sizeof(buf), "HTTP/1.1 %d\r\nContent-Length: 0\r\n\r\n", 501);
+        writer_->write(reinterpret_cast<uint8_t*>(buf), strlen(buf));
     }
 
     return false;
